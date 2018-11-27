9462f437292a5c728b608981c1603de973cb0ed3
Andrew Nguyen <32291379+a17nguyen@users.noreply.github.com>
2018-11-26 18:42:48 -0800

    updated!

--- COEN174/showEvents.php
+++ COEN174/showEvents.php
@@ -8,7 +8,7 @@ function  showEvents(){
         exit;
 	}		
 	//shows the title, author, category and highlights of the books	
-	$query = oci_parse($conn, "select * from alumnievents");
+	$query = oci_parse($conn, "select * from alumnievents order by eventdate asc");
 	// Execute the query
 	oci_execute($query);
 	while (($row = oci_fetch_array($query, OCI_BOTH)) != false) {		
@@ -16,19 +16,13 @@ function  showEvents(){
 		// or the column name as an associative array index to access the colum value
 		// Use the uppercase column names for the associative array indices
 		echo "<br>";
-
 		echo "<div class=\"container\" style=\"border: 1.5px solid; border-radius: 8px; padding: 0px;\"><h2 style=\"background-color: #b50043; color: white; padding: 15px; margin: 
 0px;\">", " $row[1]";
-
         echo "<span style=\"float: right;\">EventId:", " $row[0] </span></h2></br>";
-
-		echo "<p style=\"padding: 0px 15px;\">Event Organizer:", "<font color='blue'> $row[2] </font></br>";
-
-		echo "Location:", "<font color='blue'> $row[3] </font></br>";
-
-		echo "Date:", "<font color='blue'> $row[4] </font></br>";
-
-		echo "Description:", "<font color='blue'> $row[5] </font></p></div></br>";
+		echo "<p style=\"padding: 0px 15px;\">Organizer Name:", "<font color='blue'> $row[2]  $row[3] </font></br>";
+		echo "Location:", "<font color='blue'> $row[6] </font></br>";
+		echo "Date:", "<font color='blue'> $row[7] </font></br>";
+		echo "Description:", "<font color='blue'> $row[8] </font></p></div></br>";
 		echo "<br>";	
 	}
 
